80ebf0bec45d07f4e946f89e4f154e34dd732bf0
Jakub Szwacz <jakub@szwacz.com>
2015-05-09 14:04:14 +0200

    Wrap in UMD

--- keep-posted.js
+++ keep-posted.js
@@ -1,42 +1,56 @@
-'use strict';
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.keepPosted = factory();
+    }
+}(this, function () {
+    'use strict';
+    
+    var create = function (options) {
 
-module.exports.create = function (options) {
+        options = options || {};
+        var listeners = [];
+        var mostRecentStuffToSend;
 
-    options = options || {};
-    var listeners = [];
-    var mostRecentStuffToSend;
-
-    var keepPosted = function () {
-        mostRecentStuffToSend = arguments;
-        listeners.forEach(function (callback) {
-            callback.apply(null, mostRecentStuffToSend);
-        });
-    };
-
-    keepPosted.subscribe = function (callback) {
-
-        listeners.push(callback);
-
-        if (listeners.length === 1 && options.onFirstSubscriber) {
-            options.onFirstSubscriber();
-        }
-
-        if (options.updateNewSubscribers && mostRecentStuffToSend) {
-            callback.apply(null, mostRecentStuffToSend);
-        }
-
-        var unsubscribe = function () {
-            listeners = listeners.filter(function (listener) {
-                return listener !== callback;
+        var keepPosted = function () {
+            mostRecentStuffToSend = arguments;
+            listeners.forEach(function (callback) {
+                callback.apply(null, mostRecentStuffToSend);
             });
-
-            if (listeners.length === 0 && options.onEveryoneUnsubscribed) {
-                options.onEveryoneUnsubscribed();
-            }
         };
 
-        return unsubscribe;
+        keepPosted.subscribe = function (callback) {
+
+            listeners.push(callback);
+
+            if (listeners.length === 1 && options.onFirstSubscriber) {
+                options.onFirstSubscriber();
+            }
+
+            if (options.updateNewSubscribers && mostRecentStuffToSend) {
+                callback.apply(null, mostRecentStuffToSend);
+            }
+
+            var unsubscribe = function () {
+                listeners = listeners.filter(function (listener) {
+                    return listener !== callback;
+                });
+
+                if (listeners.length === 0 && options.onEveryoneUnsubscribed) {
+                    options.onEveryoneUnsubscribed();
+                }
+            };
+
+            return unsubscribe;
+        };
+
+        return keepPosted;
     };
 
-    return keepPosted;
-};
+    return {
+        create: create
+    }
+}));
